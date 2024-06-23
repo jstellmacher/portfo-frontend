b6f9ea156f0c358c152f1adcae718b8ade9f2187
jai <jaijai.stell@gmail.com>
2024-06-22 19:36:31 -0700

    fixes clear button on blog page

--- src/components/BlogFilter.jsx
+++ src/components/BlogFilter.jsx
@@ -1,10 +1,17 @@
-// components/BlogFilter.jsx
 import React from 'react';
 
 const BlogFilter = ({ tags, activeTag, setActiveTag, filterPosts }) => {
   return (
     <div className="mb-8">
-      <h2 className="text-2xl font-bold mb-4 text-gray-900">Filter by Tags</h2>
+      <div className="flex items-center mb-4">
+        <h2 className="text-2xl font-bold text-gray-900">Filter by Tags</h2>
+        <button
+          className="ml-4 px-4 py-2 rounded-md bg-red-200 text-gray-800 focus:outline-none hover:bg-red-500"
+          onClick={() => setActiveTag(null)}
+        >
+          Clear
+        </button>
+      </div>
       <div className="flex space-x-4 overflow-x-auto">
         {tags.map((tag) => (
           <button
@@ -17,12 +24,6 @@ const BlogFilter = ({ tags, activeTag, setActiveTag, filterPosts }) => {
             {tag}
           </button>
         ))}
-        <button
-          className="px-4 py-2 rounded-md bg-red-200 text-gray-800 focus:outline-none"
-          onClick={() => setActiveTag(null)}
-        >
-          Clear
-        </button>
       </div>
     </div>
   );
